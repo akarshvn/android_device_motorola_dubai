2c8453da66973497fc8fbf72712c80620e96d8d4
akarshvn <akarsh.velan@gmail.com>
2025-08-03 13:31:01 +0530

    dubai: drop viperfx temporarily

--- device.mk
+++ device.mk
@@ -92,6 +92,3 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
-
-#For ViPER4Android FX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
